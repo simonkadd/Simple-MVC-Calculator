b4dd0f891d5ea171366587468c21ee41a6de6ccc
Simon Kaddissi <coder@simonkaddissi.com>
2018-01-24 11:35:15 +1100

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleCalculator.Models;
+
+namespace SimpleCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Maths1 am)
+        {
+            am.error = " ";
+
+            if (am.num2 == 0 && am.ops == "/")
+            {
+                am.error = "divide by zero";
+            }
+            else if (am.ops == "+")
+            {
+                am.result = am.num1 + am.num2;
+            }
+            else if (am.ops == "-")
+            {
+                am.result = am.num1 - am.num2;
+            }
+            else if (am.ops =="*")
+            {
+                am.result = am.num1 * am.num2;
+            }
+            else if (am.ops =="/")
+            {
+                am.result = am.num1 / am.num2;
+            }
+            
+            return View(am);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Maths1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCalculator.Models
+{
+    public class Maths1
+    {
+        public int result { get; set; }
+        public string ops { get; set; }
+        public string error { get; set; }
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+    }
+}
